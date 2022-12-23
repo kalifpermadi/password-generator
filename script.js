a01e35b4470938a7f5cfea98ba6f3eaa8994776e
kalifpermadi <kalif.kai.permadi@gmail.com>
2022-12-23 22:24:45 +0700

    Added background gradient and more

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var password = document.getElementById("password");
+const text = document.getElementById("text");
 
 function generatePassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 16;
+    var passwordLength = 12;
     var pass = "";
     for (var i = 0; i < passwordLength; i++) {
         var rand = Math.floor(Math.random() * chars.length);
@@ -14,4 +15,9 @@ function generatePassword() {
 function copyPassword() {
     password.select();
     document.execCommand("copy");
+    text.innerHTML = "Copied!";
+}
+
+function outFunction() {
+    setTimeout(() => {text.innerHTML = "Copy";}, 250)
 }
